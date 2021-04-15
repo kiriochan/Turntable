c79ecb3123f5ec757cd358558265108b8a58a75b
chenqi014@chinasofti.com <chenqi014@chinasofti.com>
2021-04-15 15:35:11 +0800

    我的奖品样式调整

---
+++ src/components/card.vue
@@ -0,0 +1,87 @@
+<template>
+<div class="list-item">
+    <div class="item-img">
+        <img :src="params.img" alt="">
+    </div>
+    <div class="item-info">
+        <div class="prize-name">
+            {{params.name}}
+        </div>
+        <p class="prize-time">
+        中奖时间: {{params.time}}
+        </p>
+    </div>
+    <div class="toUse" @click="toUse(params.id)">去查看</div>
+</div>
+</template>
+
+<script>
+export default {
+    props: {
+        params: {
+            type: Object,
+            required: true
+        }
+    },
+    methods: {
+        toUse(id){
+            this.$emit('to-use',id);
+        }
+    }
+}
+</script>
+
+<style scoped>
+.list-item {
+    width: 100%;
+    height: 3.17rem;
+    margin-bottom: 0.21rem;
+    background: url('../assets/images/bg-listitem.png') no-repeat;
+    background-size: 100% 100%;
+    box-sizing: border-box;
+    padding: 0.37rem;
+    display: flex;
+}
+.item-img {
+    width: 2.4rem;
+    height: 2.4rem;
+    box-sizing: border-box;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: #CFCFCF;
+    border-radius: 0.13rem;
+}
+.item-img img{
+    max-width: 100%;
+    max-height: 100%;
+}
+.item-info {
+    width: 3.52rem;
+    padding: 0.13rem 0 0.13rem 0.27rem;
+    display: flex;
+    flex-flow: column;
+    justify-content: space-between;
+}
+.prize-name{
+    font-size: 0.37rem;
+    color: #000;
+}
+.prize-time{
+    font-size: 0.32rem;
+    color: #969696;
+    transform: scale(0.83);
+    white-space: nowrap;
+    position: relative;
+    left: -0.27rem;
+}
+.toUse{
+    font-size: 0.48rem;
+    color: #fff;
+    margin-left: 0.61rem;
+    display: flex;
+    align-items: center;
+    position: relative;
+    bottom: 0.08rem;
+}
+</style>
--- src/pages/myPrize.vue
+++ src/pages/myPrize.vue
@@ -1,89 +1,56 @@
 <template>
-  <div class="my-prize">
-    <div class="prize-list">
-      <div class="list-item">
-        <div class="item-img"></div>
-        <div class="item-info">
-          <div class="prize-name">
-            奖品名称奖品名称奖品名称奖品名称奖品名称奖品名称
-          </div>
-          <p class="prize-time">
-            中奖时间: 2021-03-14 23:59
-          </p>
-        </div>
-        <div class="go">去查看</div>
-      </div>
-      <div class="list-item">
-        <div class="item-img"></div>
-        <div class="item-info">
-          <div class="prize-name">
-            卡券名称卡券名称卡券名称卡券名称卡券名称卡券名称
-          </div>
-          <p class="prize-time">
-            中奖时间: 2021-03-14 23:59
-          </p>
-        </div>
-        <div class="go">去查看</div>
-      </div>
-    </div>
+<div class="my-prize">
+  <div class="my-prize-list">
+    <card-item :params="item" 
+      v-for="(item,index) in cardList" :key="index"
+      @to-use="toUse"
+      ></card-item>
   </div>
+</div>
 </template>
 
 <script>
-export default {}
+import cardItem from '../components/card'
+export default {
+  components:{
+    cardItem
+  },
+  data () {
+    return {
+      cardList:[
+        {id:0, time:'2021-03-14 23:59', name: '礼物', img: require('@/assets/images/button-begin.png')},
+        {id:1, time:'2021-03-14 23:59', name: '抽奖券', img: require('@/assets/images/button-begin.png')},
+        {id:1, time:'2021-03-14 23:59', name: '抽奖券', img: require('@/assets/images/button-begin.png')},
+        {id:1, time:'2021-03-14 23:59', name: '抽奖券', img: require('@/assets/images/button-begin.png')},
+        {id:1, time:'2021-03-14 23:59', name: '抽奖券', img: require('@/assets/images/button-begin.png')},
+        {id:1, time:'2021-03-14 23:59', name: '抽奖券', img: require('@/assets/images/button-begin.png')},
+        {id:1, time:'2021-03-14 23:59', name: '抽奖券', img: require('@/assets/images/button-begin.png')},
+        {id:1, time:'2021-03-14 23:59', name: '抽奖券', img: require('@/assets/images/button-begin.png')},
+        {id:1, time:'2021-03-14 23:59', name: '抽奖券', img: require('@/assets/images/button-begin.png')},
+        {id:2, time:'2021-03-14 23:59', name: '抽奖券抽奖券抽奖券抽奖券抽奖券抽奖券抽奖券', img: require('@/assets/images/button-begin.png')}
+      ]
+    }
+  },
+  methods:{
+    toUse(id){
+      console.log(id)
+    }
+  }
+}
 </script>
 
 <style scoped>
-* {
-  box-sizing: border-box;
-}
 .my-prize {
+  box-sizing: border-box;
   width: 100%;
-  height: 739px;
+  height: 100vh;
   background: url('../assets/images/bg-myprize.png') no-repeat;
   background-size: 100% 100%;
-  position: relative;
+  padding: 15vh 0.43rem 0.8rem 0.43rem;
 }
-.prize-list {
-  position: absolute;
-  top: 110px;
-  left: 15px;
-  right: 15px;
-}
-.list-item {
-  height: 119px;
-  background: url('../assets/images/bg-listitem.png') no-repeat;
-  background-size: 100% 100%;
-  display: flex;
-}
-.item-img {
-  flex: 2;
-  border-radius: 5px;
-  margin: 13px 14px 16px 14px;
-  background-color: #cfcfcf;
-}
-.item-info {
-  flex: 3;
-}
-.prize-name {
-  font-size: 14px;
-  color: #000000;
-  margin-top: 20px;
-  margin-bottom: 6px;
-}
-.prize-time {
-  white-space: nowrap;
-  color: #969696ff;
-  font-size: 12px;
-  transform: scale(0.83);
-  margin-left: -11px;
-}
-.go {
-  flex: 2;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  font-size: 18px;
-  color: #fff;
+.my-prize-list {
+  width: 100%;
+  height: 81vh;
+  overflow-y: scroll;
 }
 </style>
