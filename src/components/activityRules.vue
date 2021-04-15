e9507594088dfd03e8b256c715f3412babb96869
chenqi014@chinasofti.com <chenqi014@chinasofti.com>
2021-04-15 09:22:14 +0800

    活动规则提出

---
+++ src/components/activityRules.vue
@@ -0,0 +1,75 @@
+<template>
+<div class="lucky-wheel-rules">
+    <p>活动细则</p>
+    <br />
+    <p>
+    1. 活动时间：2021年1月1日至2021年12月31日；
+    </p>
+    <br />
+    <p>
+    2. 活动期间，每人每月有3次抽奖机会，每月抽奖机会不累积叠加；
+    </p>
+    <br />
+    <p>
+    3.
+    实物奖品请在「我的奖品」中查看并领取，满减优惠券请前往中信手机银行「生活」——
+    中信商城 ——
+    个人——卡券包中查看，并在有效期内兑换或使用；手机话费将在T+5个工作日内为客户充值至中奖后所填的手机号中；
+    </p>
+    <br />
+    <p>
+    4.中奖客户须填写完整的中奖信息，由于信息填写错误导致奖品无法发放或发放不成功的视为中奖客户主动放弃获奖资格；
+    </p>
+    <br />
+    <p>
+    5.获得实物奖品的客户，每月活动结束后将通过中信银行官方微信公布；
+    </p>
+    <br />
+    <p>
+    6.客户参与活动所填写的个人信息资料只在本次活动相关范围内使用；
+    </p>
+    <br />
+    <p>
+    7.通过不当途径参加本活动的客户，我行有权终止该客户参加活动的权利并取消其获奖资格；
+    </p>
+    <br />
+    <p>
+    8.本活动由中信易家电子商务有限公司提供相关服务,客户中奖或领取权益时填写的信息仅用于奖品或权益发放使用；
+    </p>
+    <br />
+    <p>
+    9.活动中如遇问题，请拨打客服电话4000020616-1咨询；话费充值问题请拨打4001117500咨询；
+    </p>
+    <br />
+    <p>
+    10.凡参与活动的客户，即视为接受活动所有规则，在法律范围内的最终解释权归中信银行所有。
+    </p>
+</div>
+</template>
+
+<style scoped>
+.lucky-wheel-rules {
+  position: relative;
+  top: 0;
+  z-index: 2;
+  margin-top: -10%;
+  padding: 0 27px;
+  color: #feefcf;
+  border-image-source: url('../assets/images/footer-bottom.png');
+  border-image-slice: 10% 5% 3% 5% fill;
+  border-image-width: 20px;
+}
+
+.lucky-wheel-rules p {
+  letter-spacing: 1px;
+}
+
+.lucky-wheel-rules p:first-child {
+  margin-top: 30px;
+  font-size: 14px;
+}
+
+.lucky-wheel-rules p:last-child {
+  padding-bottom: 38px;
+}
+</style>
